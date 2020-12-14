62e4b19c84412fa048d2d6207aea7cfd36b61c5a
Rg <daimaldd@gmail.com>
2020-12-14 10:54:09 +0800

    add tx

--- src/bucket.rs
+++ src/bucket.rs
@@ -3,7 +3,7 @@ const MAX_KEY_SIZE: u64 = 32768;
 /// The maximum length of a value, in bytes.
 const MAX_VALUE_SIZE: u64 = (1 << 31) - 2;
 
-const MaxUint: u64 = 0;
+const MAX_UINT: u64 = 0;
 const MIN_UINT: u64 = 0;
 const MAX_INT: i64 = 0;
 const MIN_INT: i64 = 0;
--- src/tx.rs
+++ src/tx.rs
@@ -1,1 +1,51 @@
+use crate::db::DB;
+
+/// Represents the internal transaction identifier
 pub type TxId = u64;
+
+/// Represents a `read-only` or `read-write` transaction on the database.
+/// `Read-Only` transactions can be user for retrieving values for keys and creating cursors.
+/// `Read/Write` transactions can create and remove buckets and create and remove keys.
+///
+/// *IMPORTANT*: You must commit or rollback transactions when you are done with
+/// Them. Pages can not be reclaimed by the writer until no more transactions
+/// are using them. A long running read transaction can cause the database to
+/// quickly grow.
+struct TX {
+    write_able: bool,
+    managed: bool,
+    db: DB,
+
+    // write-flag specifies the flag for write-reload methods like write_to().
+    // TX opens the database file with th specified flag to copy the data.
+    //
+    // By default, the flag is unset, which works well for mostly in-memory
+    // workloads. For database that are much larger than available RAM,
+    // set the flag to syscall.O_DIRECT to avoid trashing the page cache.
+    write_flag: isize,
+}
+
+impl TX {
+    // pub fn id(&self) -> isize {
+    //
+    // }
+    /// Return a reference to the database that created the transaction
+    pub fn db(&self) -> &DB {
+        &self.db
+    }
+
+    /// Return a mut reference to the database that created that transaction
+    pub fn db_mut(&mut self) -> &mut DB {
+        &mut self.db
+    }
+
+    // /// Return current database size in bytes as seen by this transaction.
+    // pub fn size(&self) -> isize {
+    //     self.size
+    // }
+
+    /// Return whether the transaction can perform write operations
+    pub fn write_able(&self) -> bool {
+        self.write_able
+    }
+}
